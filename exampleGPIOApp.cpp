a99cb70f51d1359b2f4f95df2dd971310db20ff6
RoboEvangelist <elpot_15@hotmail.com>
2017-07-06 04:04:45 +0000

    added code to move robot back and forward at full speed

--- exampleGPIOApp.cpp
+++ exampleGPIOApp.cpp
@@ -4,64 +4,73 @@
 #include <string>
 #include <unistd.h>
 #include "jetsonGPIO.h"
-using namespace std;
 
 int main(int argc, char *argv[]){
 
-    cout << "Testing the GPIO Pins" << endl;
+    std::cout << "Testing the GPIO Pins" << std::endl;
 
-    jetsonGPIO redLED = gpio165 ;
-    jetsonGPIO pushButton = gpio166 ;
+    jetsonGPIO redLED = gpio165;       ///< gpio165 from Jetson J3A2 pin 55
+    jetsonGPIO pushButton = gpio166;   ///< gpio166 from Jetson J3A2 pin 58
     // Make the button and led available in user space
     gpioExport(pushButton) ;
     gpioExport(redLED) ;
-    gpioSetDirection(pushButton,inputPin) ;
-    gpioSetDirection(redLED,outputPin) ;
+    gpioSetDirection(pushButton, outputPin) ;
+    gpioSetDirection(redLED, outputPin) ;
+    //gpioSetValue(redLED, on);
     // Reverse the button wiring; this is for when the button is wired
     // with a pull up resistor
     // gpioActiveLow(pushButton, true);
 
 
+    unsigned int micro_sec_delay = 1000000;
+    //gpioSetValue(redLED, on);
+    usleep(3000000);         // off for 200ms
     // Flash the LED 5 times
-    for(int i=0; i<5; i++){
-        cout << "Setting the LED on" << endl;
+    for (unsigned char i = 0; i < 5; i++) {
+        /// Move car backguards
+        std::cout << "Move Car Backguard" << std::endl;
         gpioSetValue(redLED, on);
-        usleep(200000);         // on for 200ms
-        cout << "Setting the LED off" << endl;
+        usleep(micro_sec_delay);         // on for 200ms
+        std::cout << "Stop Car" << std::endl;
         gpioSetValue(redLED, off);
-        usleep(200000);         // off for 200ms
+        usleep(micro_sec_delay);         // off for 200ms
+        /// Move car forwards
+        std::cout << "Move Car Forward" << std::endl;
+        gpioSetValue(pushButton, on);
+        usleep(micro_sec_delay);         // on for 200ms
+        std::cout << "Stop Car" << std::endl;
+        gpioSetValue(pushButton, off);
+        usleep(micro_sec_delay);         // off for 200ms
     }
 
-    // Wait for the push button to be pressed
-    cout << "Please press the button!" << endl;
+//    // Wait for the push button to be pressed
+//    std::cout << "Please press the button!" << std::endl;
+//
+//    unsigned int value = low;
+//    int ledValue = low ;
+//    // Turn off the LED
+//    gpioSetValue(redLED,low) ;
+//    for (int i = 0 ; i < 10000 ; i++) {
+//        gpioGetValue(pushButton, &value) ;
+//        // Useful for debugging
+//        // std::cout << "Button " << value << std::endl;
+//        if (value==high && ledValue != high) {
+//            // button is pressed ; turn the LED on
+//            ledValue = high ;
+//            gpioSetValue(redLED,on) ;
+//        } else {
+//            // button is *not* pressed ; turn the LED off
+//            if (ledValue != low) {
+//                ledValue = low ;
+//                gpioSetValue(redLED,off) ;
+//            }
+//
+//        }
+//        usleep(1000); // sleep for a millisecond
+//    }
 
-    unsigned int value = low;
-    int ledValue = low ;
-    // Turn off the LED
-    gpioSetValue(redLED,low) ;
-    for (int i = 0 ; i < 10000 ; i++) {
-        gpioGetValue(pushButton, &value) ;
-        // Useful for debugging
-        // cout << "Button " << value << endl;
-        if (value==high && ledValue != high) {
-            // button is pressed ; turn the LED on
-            ledValue = high ;
-            gpioSetValue(redLED,on) ;
-        } else {
-            // button is *not* pressed ; turn the LED off
-            if (ledValue != low) {
-                ledValue = low ;
-                gpioSetValue(redLED,off) ;
-            }
-
-        }
-        usleep(1000); // sleep for a millisecond
-    }
-
-    cout << "GPIO example finished." << endl;
+    std::cout << "GPIO example finished." << std::endl;
     gpioUnexport(redLED);     // unexport the LED
     gpioExport(pushButton);      // unexport the push button
     return 0;
 }
-
-
