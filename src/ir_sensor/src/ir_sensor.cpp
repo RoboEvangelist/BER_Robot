f6bd9d81c02ee796e2da08733c8945a74af9499c
RoboEvangelist <elpot_15@hotmail.com>
2018-04-01 20:20:57 +0000

    getting the current time of reading

--- src/ir_sensor/src/ir_sensor.cpp
+++ src/ir_sensor/src/ir_sensor.cpp
@@ -16,10 +16,13 @@ float IrSensor::get_range() {
 
 void IrSensor::publish_range() {
   /// TODO: pass the port number as agument to get_range()
-  //range_msg_.header.stamp = nh_.now();
   range_msg_left_.range = get_range();
   range_msg_center_.range = get_range();
   range_msg_right_.range = get_range();
+
+  range_msg_left_.header.stamp = ros::Time::now();
+  range_msg_center_.header.stamp = ros::Time::now();
+  range_msg_right_.header.stamp = ros::Time::now();
 
   /// publish the readings from all three IR sesors
   pub_range_left_.publish(range_msg_left_);
