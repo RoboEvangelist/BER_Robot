e93b1a51b79f488f2d5c7e26041ac2cc853454df
RoboEvangelist <elpot_15@hotmail.com>
2018-05-16 01:40:13 -0700

    added the protoc installations. Adde image subscriber to feed into object detection

--- src/learning_image_transport/scripts/models/research/object_detection/object_detection_tutorial.py
+++ src/learning_image_transport/scripts/models/research/object_detection/object_detection_tutorial.py
@@ -13,37 +13,16 @@ from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 
+import roslib; roslib.load_manifest('learning_image_transport')
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 import cv2
-cap = cv2.VideoCapture(1)
-#gst_str = ("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR")
-#cap = cv2.VideoCapture(gst_str)
-
-# This is needed since the notebook is stored in the object_detection folder.
-#sys.path.append("..")
-
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-# In[3]:
 
 from utils import label_map_util
-
 from utils import visualization_utils as vis_util
-
-
-# # Model preparation 
-
-# ## Variables
-# 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
-# 
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-
-# In[4]:
 
 # What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
@@ -51,30 +30,18 @@ MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+MODEL_PATH = os.path.join(os.path.dirname(sys.path[0]), \
+    'object_detection', MODEL_NAME)
+#PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_CKPT = MODEL_PATH + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+LABEL_NAME = 'mscoco_label_map.pbtxt'
+#PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join(os.path.dirname(sys.path[0]), \
+    'object_detection', 'data', LABEL_NAME)
 
 NUM_CLASSES = 90
-
-
-# ## Download Model
-
-# In[5]:
-
-#opener = urllib.request.URLopener()
-#opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-#tar_file = tarfile.open(MODEL_FILE)
-#for file in tar_file.getmembers():
-#  file_name = os.path.basename(file.name)
-#  if 'frozen_inference_graph.pb' in file_name:
-#    tar_file.extract(file, os.getcwd())
-
-
-# ## Load a (frozen) Tensorflow model into memory.
-
-# In[6]:
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
@@ -84,91 +51,83 @@ with detection_graph.as_default():
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
 
-
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-# In[7]:
-
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+categories = label_map_util.convert_label_map_to_categories( \
+    label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-
-
-# ## Helper code
-
-# In[8]:
 
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
-
 # # Detection
 
-# In[9]:
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
-
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
-
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('output.avi',0x00000021, 15.0, (640,480))
-out = cv2.VideoWriter('output.avi',fourcc, 15.0, (640,480))
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#out = cv2.VideoWriter('output.avi',fourcc, 15.0, (640,480))
+#counter = 1
 
-counter = 0
-
-# In[10]:
-
-rospy.init_node('object_detection', anonymous=False)
-rate = rospy.Rate(60) # 10hz
+# detect objects
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
-    while not rospy.is_shutdown():
-#    while True:
-      ret, image_np = cap.read()
-      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-      image_np_expanded = np.expand_dims(image_np, axis=0)
-      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-      # Each box represents a part of the image where a particular object was detected.
-      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-      # Each score represent how level of confidence for each of the objects.
-      # Score is shown on the result image, together with the class label.
-      scores = detection_graph.get_tensor_by_name('detection_scores:0')
-      classes = detection_graph.get_tensor_by_name('detection_classes:0')
-      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-      # Actual detection.
-      (boxes, scores, classes, num_detections) = sess.run(
-          [boxes, scores, classes, num_detections],
-          feed_dict={image_tensor: image_np_expanded})
-      # Visualization of the results of a detection.
-      vis_util.visualize_boxes_and_labels_on_image_array(
-          image_np,
-          np.squeeze(boxes),
-          np.squeeze(classes).astype(np.int32),
-          np.squeeze(scores),
-          category_index,
-          use_normalized_coordinates=True,
-          line_thickness=8)
+    class image_converter:
+      def __init__(self):
+        # publishes the images after objects are detected
+        self.image_pub = rospy.Publisher("image_topic_2", Image, queue_size=1)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/csi_cam/image_raw", Image, \
+            self.callback)
+    
+      def callback(self,data):
+        try:
+          cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+          print(e)
+        # tensorflow needs RGB, so convert here 
+        image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+        # the array based representation of the image will be used later
+        #  in order to prepare the
+        # result image with boxes and labels on it.
+        image_np = np.asarray(image)
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        # Actual detection.
+        (boxes, scores, classes, num_detections) = sess.run(
+            [boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+        # Visualization of the results of a detection.
+        vis_util.visualize_boxes_and_labels_on_image_array(
+            image_np,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=6)
+    
+        try:
+          self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+        except CvBridgeError as e:
+          print(e)
 
-#      cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
-      # write the flipped frame
-      out.write(image_np)
-      if cv2.waitKey(25) & 0xFF == ord('q'):
-#        cv2.destroyAllWindows()
-        break
-      if counter >= 200:
-        break
-      counter += 1
-      rate.sleep()
-    cap.release()
-out.release()
+def main(args):
+    ic = image_converter()
+    rospy.init_node('object_detection', anonymous=False)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
 
