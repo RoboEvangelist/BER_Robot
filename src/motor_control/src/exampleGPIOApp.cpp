eb5bbb92579187c81b03e8d9067f45765d8235d4
RoboEvangelist <elpot_15@hotmail.com>
2018-05-16 22:50:07 -0700

    clearing the variables if not humans detected

--- src/motor_control/src/exampleGPIOApp.cpp
+++ src/motor_control/src/exampleGPIOApp.cpp
@@ -9,11 +9,18 @@
 #include "vision_msgs/Detection2DArray.h"
 
 #include <sstream>
+#include <cmath>
 
 uint32_t box_center_x = 0;
 uint32_t box_center_y = 0;
 uint32_t box_width = 0;
 uint32_t box_height = 0;
+float box_area= 0;
+
+float image_area = 0;
+float image_center = 0;
+
+int32_t bbox_center_to_img_center_distance = 0;
 
 void reset_bbox() {
   box_center_x = 0;
@@ -25,19 +32,26 @@ void reset_bbox() {
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-void object_dection_callback(const vision_msgs::Detection2DArray::ConstPtr& msg) {
+void object_dection_callback(
+  const vision_msgs::Detection2DArray::ConstPtr& msg) {
   if (msg->detections.size()) {
+    /// area of the camera image
+    image_area = msg->detections[0].source_img.height *
+      msg->detections[0].source_img.width;
+    image_center = msg->detections[0].source_img.width / 2;
+
     for (const auto detections: msg->detections) {
       box_center_x = detections.bbox.center.x;
       box_center_y = detections.bbox.center.y;
       box_width = detections.bbox.size_x;
       box_height = detections.bbox.size_y;
+      box_area = box_width * box_height;
+      /// compute how far in pixels a person is from center of image
+      bbox_center_to_img_center_distance = box_center_x - image_center;
     }
   }
-  else {
+  else
     reset_bbox();
-    ROS_INFO("No Objects");
-  }
 }
 
 int main(int argc, char** argv) {
@@ -56,16 +70,45 @@ int main(int argc, char** argv) {
   /// detections runs at about 4Hz
   ros::Subscriber sub = nh.subscribe("/objects", 5, object_dection_callback);
   /// send 30 messages per second
-  ros::Rate loop_rate(30);
+  ros::Rate loop_rate(10);
+  
+  uint32_t distance_from_center_threshold = 0; 
+  float kMaxDistanceToImgCenterRatio = 1.0/5;
+  float kMaxObjectAreaRatio = 1.0/2;
+  float kMinObjectAreaRatio = 1.0/4;
+  /**
+   * This is a message object. You stuff it with data, and then publish it.
+   */
+  std_msgs::String msg;
 
   while (ros::ok()) {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    std_msgs::String msg;
-
     std::stringstream ss;
-    ss << "forward";
+    distance_from_center_threshold =
+      (image_center * kMaxDistanceToImgCenterRatio);
+    if ((distance_from_center_threshold > 0) &&
+      (std::abs(bbox_center_to_img_center_distance) > distance_from_center_threshold)) {
+      if (box_center_x > image_center) {
+        ss << "Turn Right";
+      }
+      else if (box_center_x < image_center) {
+        ss << "Trun Left";
+      }
+      else if ((box_area / image_area) > kMaxObjectAreaRatio) {
+        /// if object is too big, move back
+        ss << "Move Back";
+      }
+      else if ((box_area / image_area) < kMinObjectAreaRatio) {
+        /// if object is too small, move forward
+        ss << "Move Forward";
+      }
+      else {
+        ss << "Stop";
+      }
+    }
+    else {
+      ss << "Stop";
+    }
+
     msg.data = ss.str();
 
     /**
