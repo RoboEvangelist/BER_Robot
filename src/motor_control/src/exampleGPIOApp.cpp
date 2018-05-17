efb390eeda47d54eab8b1e0fd1054f29fbd15cd9
RoboEvangelist <elpot_15@hotmail.com>
2018-05-16 17:09:48 -0700

    added support for c++ 11 and subscribing to objects

--- src/motor_control/src/exampleGPIOApp.cpp
+++ src/motor_control/src/exampleGPIOApp.cpp
@@ -2,50 +2,83 @@
 
 #include <motor_control/motor_control.hpp>
 
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "vision_msgs/ObjectHypothesisWithPose.h"
+#include "vision_msgs/Detection2D.h"
+#include "vision_msgs/Detection2DArray.h"
+
+#include <sstream>
+
+uint32_t box_center_x = 0;
+uint32_t box_center_y = 0;
+uint32_t box_width = 0;
+uint32_t box_height = 0;
+
+void reset_bbox() {
+  box_center_x = 0;
+  box_center_y = 0;
+  box_width = 0;
+  box_height = 0;
+}
+
+/**
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
+ */
+void object_dection_callback(const vision_msgs::Detection2DArray::ConstPtr& msg) {
+  if (msg->detections.size()) {
+    for (const auto detections: msg->detections) {
+      box_center_x = detections.bbox.center.x;
+      box_center_y = detections.bbox.center.y;
+      box_width = detections.bbox.size_x;
+      box_height = detections.bbox.size_y;
+    }
+  }
+  else {
+    reset_bbox()
+    ROS_INFO("No Objects");
+  }
+}
+
 int main(int argc, char** argv) {
-
   ros::init(argc, argv, "motor_control");
-  std::cout << "Testing the GPIO Pins" << std::endl;
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle nh;
+  /// publisher to publish motor control commands
+  ros::Publisher car_direction_pub =
+    nh.advertise<std_msgs::String>("car_direction", 1);
+  /// subscriver to the object detection
+  /// let's set the queue size to five for now because the python object
+  /// detections runs at about 4Hz
+  ros::Subscriber sub = nh.subscribe("/objects", 5, object_dection_callback);
+  /// send 30 messages per second
+  ros::Rate loop_rate(30);
 
-  /// initialize motor control object
-  MotorControl motor_object();
+  while (ros::ok()) {
+    /**
+     * This is a message object. You stuff it with data, and then publish it.
+     */
+    std_msgs::String msg;
 
-  /// Thse pins are both, I/O, and PWM pins on Jetson Tk1
-  //jetsonGPIO redLED = gpio165;       ///< gpio165 from Jetson J3A2 pin 55 pwm2
-  //jetsonGPIO pushButton = gpio166;   ///< gpio166 from Jetson J3A2 pin 58 pwm3
-  
-  /// Jetson Tx2 pins
-//  jetsonTX1GPIONumber redLED = gpio219 ;     ///< pin 29 in J21
-//  jetsonTX1GPIONumber pushButton = gpio187 ;  ///< pin 13 in J21
-//
-//  // Make the button and led available in user space
-//  gpioExport(pushButton) ;
-//  gpioExport(redLED) ;
-//  gpioSetDirection(pushButton, outputPin) ;
-//  gpioSetDirection(redLED, outputPin) ;
-//
-//  unsigned int micro_sec_delay = 15000;
-//  usleep(3000000);         // off for 200ms
-//  // Flash the LED 5 times
-//  for (unsigned char i = 0; i < 200; i++) {
-//      /// Move car backguards
-//      gpioSetValue(redLED, on);
-//      usleep(micro_sec_delay);         // on for 200ms
-//      gpioSetValue(redLED, off);
-//      usleep(micro_sec_delay);         // off for 200ms
-//      /// Move car forwards
-//      gpioSetValue(redLED, on);
-//      usleep(micro_sec_delay);         // on for 200ms
-//      //gpioSetValue(pushButton, off);
-//      gpioSetValue(redLED, off);
-//      usleep(micro_sec_delay);         // off for 200ms
-//  }
-//
-//  std::cout << "GPIO example finished." << std::endl;
-//  gpioUnexport(redLED);     // unexport the LED
-//  gpioExport(pushButton);      // unexport the push button
+    std::stringstream ss;
+    ss << "forward";
+    msg.data = ss.str();
 
-  ros::spin();
+    /**
+     * The publish() function is how you send messages. The parameter
+     * is the message object. The type of this object must agree with the type
+     * given as a template parameter to the advertise<>() call, as was done
+     * in the constructor above.
+     */
+    car_direction_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
   return 0;
 }
 
